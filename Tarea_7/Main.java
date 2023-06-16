130db0c0be67e4e834022d109271321d0bc8911a
IamDiomer <132632330+IamDiomer@users.noreply.github.com>
2023-06-16 16:30:42 -0500

    Add files via upload

---
+++ Tarea_7/Main.java
@@ -0,0 +1,38 @@
+package Tarea_7;
+
+public class Main {
+    public static void main(String[] args) {
+        //INGRESAR DATOS
+        Rol rol = new Rol();
+        rol.agregarRolPermiso(new RolPermiso(new Rol(1231,"Administrador"),
+                new Permiso(24234,"Agregar")));
+        rol.agregarRolPermiso(new RolPermiso(new Rol(35234,"Gerente General"),
+                new Permiso(32367,"Editar")));
+        rol.agregarRolPermiso(new RolPermiso(new Rol(23465,"Coordinador"),
+                new Permiso(14428,"Eliminar")));
+
+        Permiso id = new Permiso(323);
+        Permiso nombre = new Permiso("Agregar");
+
+        /*
+        //LISTAR DATOS
+        rol.listarRolPermiso();
+
+        //BUSCAR POR NOMBRE DEL PERMISO
+        System.out.println(rol.buscarPermisoNombre(nombre));
+
+        //BUSCAR POR ID DEL PERMISO
+        System.out.println(rol.buscarPermisoId(id));
+
+        //EDITAR DATO POR ID DEL PERMISO
+        //rol.editarDato(id);
+
+        //ELIMINAR DATO
+        rol.eliminarDato(id);
+        */
+
+        rol.ordenarPermisoId();
+    }
+
+}
+
---
+++ Tarea_7/Permiso.java
@@ -0,0 +1,34 @@
+package Tarea_7;
+
+public class Permiso {
+    private int id;
+    private String nombre;
+    public Permiso(){
+    }
+    public Permiso(String nombre){
+        this.nombre = nombre;
+    }
+    public Permiso(int id){
+        this.id = id;
+    }
+    public Permiso(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
